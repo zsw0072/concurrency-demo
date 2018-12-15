1391c5a0998b760a4f0b826c779799176721f1f8
ArfaCheung <arfa.cheung.069@hotmail.com>
2018-12-16 00:30:18 +0800

    concurrency demo

---
+++ src/main/java/com/concurrency/concurrencydemo/ConcurrencyDemoApplication.java
@@ -0,0 +1,14 @@
+package com.concurrency.concurrencydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyDemoApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/concurrency/concurrencydemo/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.concurrency.concurrencydemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConcurrencyDemoApplication.class);
+    }
+
+}
+
